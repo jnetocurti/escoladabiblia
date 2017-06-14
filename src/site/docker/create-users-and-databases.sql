855a3dec281a791add5745e4b395854cfc26c596
jnetocurti <jnetocurti@gmail.com>
2017-06-14 16:07:48 -0300

    Ajuste scripts de criação do banco e application.properties (postgrtesqlhost)

--- src/site/docker/create-users-and-databases.sql
+++ src/site/docker/create-users-and-databases.sql
@@ -1,8 +1,14 @@
--- Criando um novo usu·rio
+-- Criando um novo usu√°rio
 CREATE USER escoladabiblia WITH PASSWORD 'escoladabiblia';
 
 -- Criando uma nova base de dados
 CREATE DATABASE escoladabiblia;
 
--- Concedendo todos previlÈgios para novo usu·rio na base de dados
+-- Concedendo todos previl√©gios para novo usu√°rio na base de dados
 GRANT ALL PRIVILEGES ON DATABASE escoladabiblia TO escoladabiblia;
+
+-- Criando uma nova base de dados para testes
+CREATE DATABASE escoladabiblia_teste;
+
+-- Concedendo todos previl√©gios para novo usu√°rio na base de dados de testes
+GRANT ALL PRIVILEGES ON DATABASE escoladabiblia_teste TO escoladabiblia;
