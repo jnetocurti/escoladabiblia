ab838e58348f3555f54671aba30bea1a3e6c6d85
jnetocurti <jnetocurti@gmail.com>
2018-03-24 16:31:46 -0300

    bat para backup postgresql

---
+++ src/site/env/winsw/postgresqlBackup.bat
@@ -0,0 +1,15 @@
+﻿@echo off
+for /f "tokens=1-4 delims=/ " %%i in ("%date%") do (
+  set dow=%%i
+  set month=%%j
+  set day=%%k
+  set year=%%l
+)
+set datestr=%dow%_%month%
+echo datestr is %datestr%
+ 
+set BACKUP_FILE=escoladabiblia_%datestr%.backup
+echo backup file name is %BACKUP_FILE%
+SET PGPASSWORD=@post!EB
+echo on0
+pg_dump -i -h localhost -p 5432 -U postgres -F c -b -v -f %BACKUP_FILE% escoladabiblia
